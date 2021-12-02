3d0507e7ce945bfd57340f8e534b0a14084b60e7
Pete Davis <pete.davis@rcn.org.uk>
2021-12-02 12:13:55 +0000

    Initial js examples

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
+import { Header } from "../components/Header";
+
 export default function Home() {
-  return <main>Hello devjam</main>;
+  return (
+    <main>
+      <Header />
+      <h1>Hello</h1>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </main>
+  );
 }
