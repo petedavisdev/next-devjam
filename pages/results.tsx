35a70e24cf3386b2f43db66d36d49c33ca996506
Pete Davis <pete.davis@rcn.org.uk>
2021-12-08 16:12:03 +0000

    Fix ts error

--- pages/results.tsx
+++ pages/results.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { chocolateList } from "../data/chocolateList";
+import { chocolateList, ChocolateListItem } from "../data/chocolateList";
 
 export default function ChocolateResults() {
 	const scores = [2, 1, 0, 3];
@@ -12,7 +12,7 @@ export default function ChocolateResults() {
 	console.log(scoredChoices);
 
 	const resultsList = scoredChoices.map(
-		([score, { name, imageUrl }], index) => (
+		([score, { name, imageUrl }]: any, index) => (
 			<li key={index}>
 				<Image src={imageUrl} alt={name} width={80} height={80} />
 				{score}
